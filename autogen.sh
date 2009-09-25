bc894a47e6b50ecd6d800871bd03c6c0486badf6
Daniel P. Berrange <berrange@redhat.com>
2009-09-25 19:51:12 +0100

    Split out compile of frontend and backend
    Split the frontend and backend out into separate build libraries.
    This ensures only the backend can use libgphoto2 directly and
    that the backend doesn't accidentally end up depending on frontend

--- autogen.sh
+++ autogen.sh
@@ -31,15 +31,13 @@ if test "$DIE" -eq 1; then
 fi
 
 if test -z "$*"; then
-	echo "I am going to run ./configure with --enable-warnings - if you "
-        echo "wish to pass any extra arguments to it, please specify them on "
-        echo "the $0 command line."
+	echo "I am going to run ./configure with --enable-compile-warnings=error"
+        echo "If you wish to pass any extra arguments to it, please specify them"
+        echo "on the $0 command line."
 fi
 
-aclocal
-autoheader
-automake --add-missing --copy
-autoconf
+mkdir -p m4
+autoreconf -if
 
 cd $THEDIR
 
