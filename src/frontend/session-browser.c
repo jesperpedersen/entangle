bb3300c6212af5e45bb620a2ece3f3b75317a6ca
Daniel P. Berrange <berrange@redhat.com>
2009-10-05 21:24:22 +0100

    Sort thumbnails based on last modified date

--- src/frontend/session-browser.c
+++ src/frontend/session-browser.c
@@ -58,12 +58,13 @@ static void do_model_refresh(CapaSessionBrowser *browser)
   for (int i = 0 ; i < capa_session_image_count(priv->session) ; i++) {
     CapaImage *img = capa_session_image_get(priv->session, i);
     GdkPixbuf *pixbuf = capa_image_thumbnail(img);
+    int mod = capa_image_last_modified(img);
     GtkTreeIter iter;
 
     gtk_list_store_append(priv->model, &iter);
 
     /* XXX what's our refcount policy going to be for pixbuf.... */
-    gtk_list_store_set(priv->model, &iter, 0, img, 1, pixbuf, -1);
+    gtk_list_store_set(priv->model, &iter, 0, img, 1, pixbuf, 2, mod, -1);
 
     //g_object_unref(cam);
   }
@@ -78,16 +79,33 @@ static void do_image_added(CapaSession *session G_GNUC_UNUSED,
   CapaSessionBrowserPrivate *priv = manager->priv;
   GdkPixbuf *pixbuf = capa_image_thumbnail(img);
   GtkTreeIter iter;
+  int mod = capa_image_last_modified(img);
 
   fprintf(stderr, "Got new image for model %s %p\n", capa_image_filename(img), priv);
 
   gtk_list_store_append(priv->model, &iter);
 
     /* XXX what's our refcount policy going to be for pixbuf.... */
-  gtk_list_store_set(priv->model, &iter, 0, img, 1, pixbuf, -1);
+  gtk_list_store_set(priv->model, &iter, 0, img, 1, pixbuf, 2, mod, -1);
 
   gtk_widget_queue_resize(GTK_WIDGET(manager));
 }
+
+
+static gint
+do_image_sort_modified(GtkTreeModel *model,
+		       GtkTreeIter  *a,
+		       GtkTreeIter  *b,
+		       gpointer data G_GNUC_UNUSED)
+{
+  gint ai, bi;
+
+  gtk_tree_model_get(model, a, 2, &ai, -1);
+  gtk_tree_model_get(model, b, 2, &bi, -1);
+
+  return ai - bi;
+}
+
 
 
 static void capa_session_browser_get_property(GObject *object,
@@ -189,12 +207,18 @@ static void capa_session_browser_init(CapaSessionBrowser *browser)
   priv = browser->priv = CAPA_SESSION_BROWSER_GET_PRIVATE(browser);
   memset(priv, 0, sizeof *priv);
 
-  priv->model = gtk_list_store_new(2, CAPA_TYPE_IMAGE, GDK_TYPE_PIXBUF);
+  priv->model = gtk_list_store_new(3, CAPA_TYPE_IMAGE, GDK_TYPE_PIXBUF, G_TYPE_INT);
 
   gtk_icon_view_set_text_column(GTK_ICON_VIEW(browser), -1);
   gtk_icon_view_set_pixbuf_column(GTK_ICON_VIEW(browser), 1);
   gtk_icon_view_set_selection_mode(GTK_ICON_VIEW(browser), GTK_SELECTION_SINGLE);
   gtk_icon_view_set_model(GTK_ICON_VIEW(browser), GTK_TREE_MODEL(priv->model));
+
+  gtk_tree_sortable_set_default_sort_func(GTK_TREE_SORTABLE(priv->model),
+					  do_image_sort_modified, NULL, NULL);
+  gtk_tree_sortable_set_sort_column_id(GTK_TREE_SORTABLE(priv->model),
+				       GTK_TREE_SORTABLE_DEFAULT_SORT_COLUMN_ID,
+				       GTK_SORT_ASCENDING);
 
   gtk_icon_view_set_orientation(GTK_ICON_VIEW(browser), GTK_ORIENTATION_HORIZONTAL);
   /* XXX gross hack - GtkIconView doesn't seem to have a better
