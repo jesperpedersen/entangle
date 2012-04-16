8d845485dc1b1282241844e8e39acebcb55d6dc4
Daniel P. Berrange <berrange@redhat.com>
2012-04-16 23:20:51 +0100

    Add --system arg support to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -44,7 +44,19 @@ autoreconf -if
 
 cd $THEDIR
 
-$srcdir/configure --enable-werror "$@" && {
+if test "x$1" = "x--system"; then
+    shift
+    prefix=/usr
+    libdir=$prefix/lib
+    sysconfdir=/etc
+    localstatedir=/var
+    if [ -d /usr/lib64 ]; then
+      libdir=$prefix/lib64
+    fi
+    EXTRA_ARGS="--prefix=$prefix --sysconfdir=$sysconfdir --localstatedir=$localstatedir --libdir=$libdir"
+fi
+
+$srcdir/configure $EXTRA_ARGS "$@" && {
     echo
     echo "Now type 'make' to compile entangle."
 }
