jesperpedersen/entangle

commit 9a977e23d9f399b8ae2afab98cddec0022e5a276

Allow camera to be NULL in control panel

src/frontend/entangle-control-panel.c
@@ -712,7 +712,7 @@ void entangle_control_panel_set_camera(EntangleControlPanel *panel,
                                        EntangleCamera *cam)
 {
     g_return_if_fail(ENTANGLE_IS_CONTROL_PANEL(panel));
-    g_return_if_fail(ENTANGLE_IS_CAMERA(cam));
+    g_return_if_fail(!cam || ENTANGLE_IS_CAMERA(cam));
 
     EntangleControlPanelPrivate *priv = panel->priv;
 

commit 5739f330f154e8c6c822a660605038764d80dacf

Allow camera to be NULL
Re-add 9a977e23d9f399b8ae2afab98cddec0022e5a276 accidentally
reverted

src/frontend/entangle-control-panel.c
@@ -708,7 +708,7 @@ void entangle_control_panel_set_camera(EntangleControlPanel *panel,
                                        EntangleCamera *cam)
 {
     g_return_if_fail(ENTANGLE_IS_CONTROL_PANEL(panel));
-    g_return_if_fail(ENTANGLE_IS_CAMERA(cam));
+    g_return_if_fail(!cam || ENTANGLE_IS_CAMERA(cam));
 
     EntangleControlPanelPrivate *priv = panel->priv;
 