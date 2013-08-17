0da392f7350566e1c5f4fbf4f742fd7214392f93
Daniel P. Berrange <berrange@redhat.com>
2013-08-17 19:26:56 +0100

    Remove use of 'document_mode' field in raw file generation
    LibRaw >= 0.15 has removed the 'document_mode' field, so we must
    stop using it in Entangle. Fortunately it wasn't doing anything
    important for us.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- src/backend/entangle-pixbuf.c
+++ src/backend/entangle-pixbuf.c
@@ -150,7 +150,6 @@ static GdkPixbuf *entangle_pixbuf_open_image_master_raw(EntangleImage *image)
     raw->params.use_camera_wb = 1;
     raw->params.use_auto_wb = 1;
     raw->params.use_camera_matrix = 1;
-    raw->params.document_mode = 0;
     raw->params.user_qual = 0;
     raw->params.fbdd_noiserd = 1;
 
