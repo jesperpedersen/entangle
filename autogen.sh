78fc115a2be834b9c9cc519297b9d5530b7f9913
Daniel P. Berrange <berrange@redhat.com>
2013-08-17 19:09:51 +0100

    Switch to automatically generated AUTHORS file
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- autogen.sh
+++ autogen.sh
@@ -36,6 +36,11 @@ if test -z "$*"; then
         echo "on the $0 command line."
 fi
 
+# Real ChangeLog/AUTHORS is auto-generated from GIT logs at
+# make dist time, but automake requires that it
+# exists at all times :-(
+touch ChangeLog AUTHORS
+
 mkdir -p m4
 
 intltoolize --force
